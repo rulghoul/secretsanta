13336850ce2179a72208157ebf05b2d152ca6837
Raul Perez <ghoulrul@gmail.com>
2023-11-14 15:52:32 -0600

    se corrige gunicornm_santa

--- gunicorn_santa.bash
+++ gunicorn_santa.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 NAME="santa"                                  # Name of the application
-DJANGODIR=/var/www/python/secretsanta/santa          # Django project directory
+DJANGODIR=/var/www/python/secretsanta          # Django project directory
 SOCKFILE=/var/www/python/secretsanta/run/gunicorn.sock  # we will communicte using this unix socket
 USER=santa                                        # the user to run as
 GROUP=webapps                                     # the group to run as
@@ -13,7 +13,7 @@ echo "Starting $NAME as `whoami`"
 
 # Activate the virtual environment
 cd $DJANGODIR
-source ../virtual/bin/activate
+source virtual/bin/activate
 
 # Create the run directory if it doesn't exist
 RUNDIR=$(dirname $SOCKFILE)
@@ -21,7 +21,7 @@ test -d $RUNDIR || mkdir -p $RUNDIR
 
 # Start your Django Unicorn
 # Programs meant to be run under supervisor should not daemonize themselves (do not use --daemon)
-exec ../virtual/bin/gunicorn ${DJANGO_WSGI_MODULE}:application \
+exec virtual/bin/gunicorn ${DJANGO_WSGI_MODULE}:application \
   --name $NAME \
   --workers $NUM_WORKERS \
   --user=$USER --group=$GROUP \
